eae9c1d04a47ac463f8c5e946f23b5f9aac27587
AbhishekAgr <abhishek.agrawal@kelltontech.com>
2018-07-31 09:21:55 +0530

    second commit

--- app.component.ts
+++ app.component.ts
@@ -12,20 +12,30 @@ export class AppComponent{
   title = 'reactiveForm';
   myFormGroup=new FormGroup({
       fname:new FormControl('',[
-        Validators.required
+        Validators.required,
+        Validators.pattern("[a-zA-Z]+$")
       ]),
       lname:new FormControl('',[
-        Validators.required]),
+        Validators.required,
+        Validators.pattern("[a-zA-Z]+$")
+      ]),
       gender:new FormControl('',[
-        Validators.required]),
+        Validators.required,
+        Validators.pattern("")
+      ]),
       contactNo:new FormControl('',[
         Validators.required,
         Validators.minLength(10),
-        Validators.maxLength(10)
+        Validators.maxLength(10),
+        Validators.pattern("[0-9]+$")
       ]),
       password:new FormControl('',[Validators.required]),
-      confirmPassword:new FormControl('',[Validators.required])
-
+      confirmPassword:new FormControl('',[Validators.required]),
+      empId:new FormControl('',[Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(4),
+      Validators.pattern("[0-9]+$")
+      ])
     }
   );
  
